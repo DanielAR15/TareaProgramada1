c2c29ae34a2f21b1ebfe0c21c911ef058e21e59b
Daniel Avilán <39519748+DanielAR15@users.noreply.github.com>
2025-09-21 16:11:44 -0600

    Implementacion de funciones (borrar, limpiar y miembro) en listaordenadadinamica.py

--- src/tarea1/listaordenadadinamica.py
+++ src/tarea1/listaordenadadinamica.py
@@ -14,14 +14,26 @@ class ListaOrdenadaDinámica(Diccionario):
         return self.__tamaño
     
     def __getitem__(self, indice):
-        pass
 
-    """
-    Funcion el diccionario 
-    """
+        """
+        Permite acceder a un elemento por índice
+        """
+
+        if indice < 0 or indice >= self.__tamaño:
+            raise IndexError("Índice fuera de rango")
+        actual = self.__cabeza.siguiente
+        for _ in range(indice):
+            actual = actual.siguiente
+        return actual.elemento  
+
+    # Metodoos del diccionario ---------------------------
 
     def inserte(self, elemento):
-  
+
+        """
+        Inserta el elemento en orden ascendente
+        """
+
         referencia: Nodo = self.__cabeza
         nodo = Nodo(elemento)
         if referencia.siguiente is None:
@@ -31,17 +43,41 @@ class ListaOrdenadaDinámica(Diccionario):
                 referencia = referencia.siguiente
             nodo.siguiente = referencia.siguiente
             referencia.siguiente = nodo
+            self.__tamaño += 1
 
     def borre(self, elemento):
 
-        pass
+        """
+        Borra un elemento si existe.
+        """
+
+        referencia: Nodo = self.__cabeza
+        while referencia.siguiente is not None and referencia.siguiente.elemento != elemento:
+            referencia = referencia.siguiente
+        if referencia.siguiente is not None:
+            referencia.siguiente = referencia.siguiente.siguiente
+            self.__tamaño -= 1
 
     def limpie(self):
+
+        """
+        Elimina todos los elementos de un diccionario.
+        """
+
+        self.__cabeza.siguiente = None
+        self.__tamaño = 0
         pass
 
     def miembro(self, elemento):
-        pass
-
+        referencia: Nodo = self.__cabeza.siguiente
+        while referencia is not None:
+            if referencia.elemento == elemento:
+                return True
+            elif referencia.elemento > elemento:
+                return False
+            referencia = referencia.siguiente
+        return False
+    
     def imprima(self):
         print(self)
 
